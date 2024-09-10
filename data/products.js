48b2b8ae75f5bd187b694ae9a299b70f7e35c701
harshan <harshan2412005@gmail.com>
2024-09-10 21:04:24 +0530

    Load Products from the backend

--- data/products.js
+++ data/products.js
@@ -132,7 +132,33 @@ export class Appliance extends Product {
 // get value of 'this' outside the arrow function inside the method.  
 
 
- 
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if(productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+    
+      } else if(productDetails.type === 'appliance') {
+        return new Appliance(productDetails);
+      }
+    
+      return new Product(productDetails);
+    });
+    console.log('Products loaded.');
+
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+};
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -836,4 +862,5 @@ export const products = [
 
   return new Product(productDetails);
 });
+*/
 // console.log(products);
