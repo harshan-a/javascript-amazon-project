636d825364a4f0711a46e314ebf1140bcd9adb45
harshan <harshan2412005@gmail.com>
2024-09-15 15:02:39 +0530

    Convert loadProducts from xhr to fetch

--- tests/checkout/orderSummaryTests.js
+++ tests/checkout/orderSummaryTests.js
@@ -1,7 +1,7 @@
 import {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
 // import {loadFromStorage, cart} from '../../data/cart.js';
 import {cart} from '../../data/cart-class.js';
-import {loadProducts} from '../../data/products.js';
+import {loadProducts, loadProductsFetch} from '../../data/products.js';
 
 
 describe('test suite: renderOrderSummary', () => {
@@ -10,7 +10,7 @@ describe('test suite: renderOrderSummary', () => {
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
 
   beforeAll((done) => {
-    loadProducts(() => {
+    loadProductsFetch().then(() => {
       done(); // after the done() called then the remaining code below will execute;
     });
   });
