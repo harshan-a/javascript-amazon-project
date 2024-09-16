7ba2ae7c0285e2591c0aed866b621b91e1d85350
harshan <harshan2412005@gmail.com>
2024-09-16 23:36:53 +0530

    Create order using the backend

---
+++ data/order.js
@@ -0,0 +1,35 @@
+const orders = JSON.parse(localStorage.getItem('order')) || [];
+
+export function loadOrder(cart) {
+  const promise = fetch('https://supersimplebackend.dev/orders', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      cart: cart
+    })
+
+  }).then((response) => {
+    console.log('load order');
+    return response.json();
+
+  }).catch((error) => {
+    console.log('Unexpected error. Try again Later.');
+  });
+
+  return promise;
+};
+
+
+export function addOrder(order) {
+  orders.unshift(order);
+  saveToLocalStorage();
+  console.log(orders);
+};
+
+
+function saveToLocalStorage() {
+  localStorage.setItem('order', JSON.stringify(orders));
+}
+
--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import {cart} from '../../data/cart-class.js';
 import {getProduct} from '../../data/products.js';
 import {getDeliveryOption} from '../../data/deliveryOptions.js';
 import {formatCurrency} from '../utils/money.js';
+import {loadOrder, addOrder} from '../../data/order.js';
 
 export function renderPaymentSummary() {
   let productsCents = 0;
@@ -65,11 +66,29 @@ export function renderPaymentSummary() {
       </div>
     </div>
 
-    <button class="place-order-button button-primary">
+    <button class="place-order-button button-primary
+      js-place-order">
       Place your order
     </button>
   `;
 
   document.querySelector('.js-payment-summary')
     .innerHTML = paymentSummaryHTML;
+
+  document.querySelector('.js-place-order')
+    .addEventListener('click', async () => {
+      try {
+        const order = await loadOrder(cart.cartItems);
+        if(!order) {
+          throw 'fetch error';
+        }
+        addOrder(order);
+
+        window.location.href = 'orders.html';
+
+      } catch (error) {
+        // console.log('Unexpected error. Try again Later.');
+        alert('Unexpected error. Try again Later.');
+      };
+    });
 };
